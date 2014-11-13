3102b3419b4d8024b380f2bff2937c284f3f30ba
Stephen Koo <stephen.theking@gmail.com>
2014-11-13 00:16:45 -0800

    Finish first draft of log loader

--- mnemosyne_logs.py
+++ mnemosyne_logs.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 conn = sqlite3.connect('data/2014-01-27-mnemosynelogs-all.db')
+conn.row_factory = sqlite3.Row
 
 CLEAN_ROW_CONDITIONS = """grade NOT NULL AND
 acq_reps NOT NULL AND
@@ -10,13 +11,24 @@ acq_reps_since_lapse NOT NULL AND
 ret_reps_since_lapse NOT NULL AND
 actual_interval NOT NULL""";
 
-
 def list_user_ids(limit=100):
     c = conn.cursor()
     c.execute('SELECT DISTINCT user_id FROM log WHERE %s LIMIT %s' % (CLEAN_ROW_CONDITIONS, limit))
     return [row[0] for row in c.fetchall()]
 
 def fetch_logs(user_id):
-    pass
+    c = conn.cursor()
+    c.execute('SELECT DISTINCT object_id FROM log WHERE user_id = "%s" AND %s' % (user_id, CLEAN_ROW_CONDITIONS))
+    object_ids = [row[0] for row in c.fetchall()]
+
+    logs = []
+    for object_id in object_ids:
+        c.execute('SELECT * FROM log WHERE user_id = "%s" AND object_id = "%s" AND %s ORDER BY timestamp'
+                % (user_id, object_id, CLEAN_ROW_CONDITIONS))
+        logs.append(c.fetchall())
+
+    return logs
+
+
 
 
