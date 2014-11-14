0ab45585fdc26488f4a084762cdea5f7fac00e04
Sheila Ramaswamy <ramasshe@cs.stanford.edu>
2014-11-14 08:26:16 -0800

    Minor modification to training_data.py (removed thinking_time (should only consider thinking_time_prev for features)

--- training_data.py
+++ training_data.py
@@ -3,21 +3,20 @@ import numpy as np
 
 CURR_GRADE = 0
 PREV_GRADE = 1
-ACQ_REPS = 9 # NO_IMPACT (was 2)
+ACQ_REPS = 8 # NO_IMPACT (was 2)
 EASINESS = 2
 RET_REPS = 3
 LAPSES = 4
-ACQ_REPS_SINCE_LAPSE = 10 #(was 5)
+ACQ_REPS_SINCE_LAPSE = 9 #(was 5)
 ACTUAL_INTERVAL = 5
 RET_REPS_SINCE_LAPSE = 6
-THINKING_TIME = 7
-THINKING_TIME_PREV = 8
+THINKING_TIME_PREV = 7
 
 
 def get_training_data(limit=1000):
     users = mnemosyne_logs.list_user_ids(limit)
     num_examples = 0
-    training_x = np.ones((limit,11), dtype=float)
+    training_x = np.ones((limit, 10), dtype=float)
     training_y = np.ones(limit, dtype=float)
     for user in users:
         logs = mnemosyne_logs.fetch_logs(user)
@@ -34,7 +33,6 @@ def get_training_data(limit=1000):
                 training_x[num_examples][LAPSES] = prev[mnemosyne_logs.LAPSES]
                 training_x[num_examples][ACTUAL_INTERVAL] = prev[mnemosyne_logs.ACTUAL_INTERVAL]
                 training_x[num_examples][RET_REPS_SINCE_LAPSE] = prev[mnemosyne_logs.RET_REPS_SINCE_LAPSE]
-                training_x[num_examples][THINKING_TIME] = curr[THINKING_TIME]
                 training_x[num_examples][THINKING_TIME_PREV] = prev[THINKING_TIME]
                 training_x[num_examples][ACQ_REPS] = prev[mnemosyne_logs.ACQ_REPS]
                 training_x[num_examples][ACQ_REPS_SINCE_LAPSE] = prev[mnemosyne_logs.ACQ_REPS_SINCE_LAPSE]
