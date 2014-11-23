2bf21dfd7c1ad890894818dd4f54158225266185
Sheila Ramaswamy <ramasshe@cs.stanford.edu>
2014-11-22 22:46:53 -0800

    small adjustments, added plots

--- cluster_db.py
+++ cluster_db.py
@@ -8,15 +8,13 @@ def create_userdb():
     c.execute("DROP TABLE users")
     c.execute('''CREATE TABLE users
     	(user_id text, retention real, lapses real)''')
-    c.execute('SELECT user_id, SUM(ret), SUM(lapse), SUM(ct) from (select user_id, (MAX(ret_reps)-MIN(ret_reps)) as ret, (MAX(lapses)-MIN(lapses)) as lapse, COUNT(*) as ct from log GROUP BY user_id, object_id) GROUP BY user_id')
+    c.execute('SELECT user_id, SUM(ret), SUM(lapse), SUM(ct) from (select user_id, (MAX(ret_reps)-MIN(ret_reps)) as ret, (MAX(lapses)-MIN(lapses)) as lapse, COUNT(*) as ct from log GROUP BY user_id, object_id) WHERE ct > 1 GROUP BY user_id')
     r = c.fetchall()
     for row in r:
         new_row = [row[0], float(row[1])/float(row[3]), float(row[2])/float(row[3])]
         if (row[1] > 500):
-            print "nyah"
             continue
         if (row[2] > 500):
-            print "nyah 2"
             continue
         c.execute("INSERT INTO users values (?,?,?)", new_row)
     new_conn.commit()
--- kmeans.py
+++ kmeans.py
@@ -12,9 +12,34 @@ def run_kmeans(num_means=8):
 	arr = np.array(c.fetchall())
 	estimator = KMeans(n_clusters=num_means)
 	estimator.fit(arr)
+	print "======================================="
+	print "1. kmeans together"
 	print estimator.cluster_centers_
 	print estimator.labels_
 	print estimator.inertia_
 	plt.plot(arr[:, 0], arr[:, 1], 'k.', markersize=2)
 	plt.show()
+
+	print "======================================="
+	print "2. kmeans retention rate"
+	c.execute("SELECT retention, lapses-lapses from users")
+	arr = np.array(c.fetchall())
+	estimator.fit(arr)
+	print estimator.cluster_centers_
+	print estimator.labels_
+	print estimator.inertia_
+	plt.hist(arr[:,0], bins=30)
+	plt.show()
+
+	print "======================================="
+	print "3. kmeans lapse rate"
+	c.execute("SELECT retention-retention, lapses from users")
+	arr = np.array(c.fetchall())
+	estimator.fit(arr)
+	print estimator.cluster_centers_
+	print estimator.labels_
+	print estimator.inertia_
+	plt.hist(arr[:,1], bins=30)
+	plt.show()
+
 
