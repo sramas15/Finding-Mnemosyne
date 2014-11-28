2fd38e61f5a12a5ad4d58e3f30f0ef2a6e222d36
Stephen Koo <stephen.theking@gmail.com>
2014-11-28 13:31:21 -0800

    Prettify login

--- webapp/views.py
+++ webapp/views.py
@@ -4,6 +4,7 @@ from django.template.loader import get_template
 from django.template import Context, RequestContext
 
 def home(request):
-    return render_to_response('home.html', 
-                              {'var': 'value'},
-                              context_instance=RequestContext(request))
+    return render_to_response('home.html', context_instance=RequestContext(request))
+
+def study(request):
+    return render_to_response('study.html', context_instance=RequestContext(request))
