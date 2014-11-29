7aeb8111efc0cbf7293548e2bcc79793cd9f0a6a
Stephen Koo <stephen.theking@gmail.com>
2014-11-29 00:38:58 -0800

    Finish filling out models

--- cards/models.py
+++ cards/models.py
@@ -12,5 +12,10 @@ class Card(models.Model):
 class AssignedCard(models.Model):
     card = models.ForeignKey(Card)
     user = models.ForeignKey(User)
-
-    # TODO: additional metadata
+    last_shown = models.DateField(auto_now=True)
+    last_grade = models.SmallIntegerField(default=0)
+    easiness = models.FloatField(default=2.5)
+    ret_reps = models.IntegerField(default=0)
+    ret_reps_since_lapse = models.IntegerField(default=0)
+    lapses = models.IntegerField(default=0)
+    scheduled_interval = models.IntegerField(default=0)
--- scheduling/models.py
+++ scheduling/models.py
@@ -2,9 +2,24 @@ from django.contrib.auth.models import User
 from django.db import models
 from cards.models import Card
 
-class RepLog(models.Model):
+class RepIntervalLog(models.Model):
     user = models.ForeignKey(User)
     card = models.ForeignKey(Card)
-    
-    # TODO: add the rest of the fields!
+    grade = models.SmallIntegerField()
+    new_grade = models.SmallIntegerField()
+    easiness = models.FloatField()
+    ret_reps = models.IntegerField()
+    ret_reps_since_lapse = models.IntegerField()
+    lapses = models.IntegerField()
+    interval = models.IntegerField()
+
+class RegressionWeights(models.Model):
+    user = models.ForeignKey(User)
+    grade = models.FloatField()
+    new_grade = models.FloatField()
+    easiness = models.FloatField()
+    ret_reps = models.FloatField()
+    ret_reps_since_lapse = models.FloatField()
+    lapses = models.FloatField()
+    interval = models.FloatField()
 
